2162b500c0404bf310753d019411bcb2442ac794
AdrienHalletUCL <adrien.hallet@student.uclouvain.be>
2018-03-14 21:58:37 +0100

    found how to bind method

--- feature_execution/feature_execution_impl.rb
+++ feature_execution/feature_execution_impl.rb
@@ -9,16 +9,11 @@ class FeatureExecutionImpl
 
   def alter(action, feature_selector)
     myobject = feature_selector.feature.get_adapter
-    parsedobject =  Object.const_get myobject
-    puts feature_selector.feature
-    puts 'Location'
-    puts feature_selector.feature.instance_methods.sort
-    puts feature_selector.feature.instance_method(:printing)
-    puts '********'
 
     raise 'Unknown method' unless feature_selector.feature.instance_methods.include?(:printing)
     mymethod = feature_selector.feature.instance_method(:printing)
-    parsedobject.define_singleton_method(mymethod)
+    Printer.send(:define_method, :printing, mymethod)
+    puts 'End'
 
 
   end
