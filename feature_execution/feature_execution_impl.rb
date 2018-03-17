039de144e537436c26c274d35d0439398c11ac70
AdrienHalletUCL <adrien.hallet@student.uclouvain.be>
2018-03-18 00:16:48 +0100

    adapt seems to work on new version

--- feature_execution/feature_execution_impl.rb
+++ feature_execution/feature_execution_impl.rb
@@ -8,116 +8,79 @@ class FeatureExecutionImpl
   include Singleton
 
   def alter(action, feature_selector)
-    id=caller[0][/`.*'/][1..-2]
-    thread = Thread.current
-    if thread[('history'+id).to_sym].nil?
-      thread[('history'+id).to_sym] = Hash.new
-      thread[('history_logs'+id).to_sym] = Hash.new      
-    end
     if action == :adapt
       adapter = Object.const_get(feature_selector.feature.get_adapter)
+      adapter.instance_variable_set(:@last_called, Hash.new(nil))
+
       proceed_body = proc do
-        key = feature_selector.feature.get_adapter.to_s
-        callname =  caller[0][/`.*'/][1..-2] #magic
-        #TODO : Access is not thread-safe, find another way to go back on proceed
-        debug = thread[('history'+id).to_sym]
-        res = thread[('history'+id).to_sym][id+key+callname]
-        unless self.instance_variable_defined? :@access
-          self.instance_variable_set(:@access, Hash.new(1))
+        myself = self
+        unless self.instance_variable_defined?(:@last_called)
+          @last_called = Hash.new(nil)
         end
-        position = self.instance_variable_get :@access
-        index = position[callname]
-        node = res[-index]
-        if node.eql? 'HEAD'
-          position[callname] = 1
-          index = position[callname]
-          node = res[-index]
+        callname = caller_locations(1,1)[0].label
+        available_methods = self.class.instance_methods(false).sort
+        candidate = nil
+        id = nil
+        available_methods.reverse_each do |method|
+          if method.to_s.start_with?(callname)
+            id = method.to_s.sub! callname, ''
+            lc = @last_called[callname]
+            boolean = lc.nil?
+            if (id.eql? '') || (lc.nil?) || (id < lc)
+              candidate = method
+              break
+            end
+          end
         end
-        position[callname] += 1
-        self.instance_variable_set(:@access, position)
-        meth = node.bind(self)
+        if candidate.nil?
+          p 'did not find :' + callname.to_s
+        else
+          if id == ''
+            @last_called[callname] = nil
+          else
+            @last_called[callname] = id
+          end
+          self.method(candidate).call
 
-
-        meth.call
+        end
       end
       adapter.send(:define_method, :proceed, &proceed_body)
+
       methods = feature_selector.feature.instance_methods
       methods.each do |method_name|
         if method_name.to_s.eql? 'proceed'
           next
         end
 
-        change_flag = false
-        begin
-          meth = adapter.instance_method(method_name) # Raise a NameError if the method does not exist
-
-          res = thread[('history'+id).to_sym][id+(adapter.to_s) +(method_name.to_s)]
-          if res.nil?
-            thread[('history'+id).to_sym][id+(adapter.to_s) +(method_name.to_s)] = []
-
-            res = thread[('history'+id).to_sym][id+(adapter.to_s) +(method_name.to_s)]
-            res.push('HEAD')
+        adapter_methods = adapter.instance_methods.sort
+        if adapter_methods.include? method_name
+          to_move = adapter.instance_method(method_name)
+          adapter.send(:remove_method, method_name)
+          id = 1
+          while adapter_methods.include? (method_name.to_s + id.to_s).to_sym
+            id += 1
           end
-          res.push(meth)
-          if thread[('history_logs'+id).to_sym][id+(adapter.to_s)+(method_name.to_s)].nil?
-            thread[('history_logs'+id).to_sym][id+(adapter.to_s)+(method_name.to_s)] = []
-          end
-          thread[('history_logs'+id).to_sym][id+(adapter.to_s)+(method_name.to_s)].push(feature_selector)
-          change_flag = true
-        rescue NameError => e #Todo Remove duplication in conditions
-
-
+          new_name = (method_name.to_s + id.to_s).to_sym
+          adapter.send(:define_method, new_name, to_move)
         end
-        if change_flag
-          feature_selector.instance_variable_set(:@change, true)
-        end
-
 
         method_body = feature_selector.feature.instance_method(method_name)
         adapter.send(:define_method, method_name , method_body)
+
       end
 
 
     elsif action == :unadapt
 
-      adapter = Object.const_get feature_selector.feature.get_adapter
-      adapter_methods = adapter.instance_methods
-      moduler = feature_selector.feature
-
-      if feature_selector.instance_variable_defined? :@change
-        added_methods = moduler.instance_methods
-        added_methods.each do |current_method|
-          if thread[('history'+id).to_sym][id+(adapter.to_s) + (current_method.to_s)].nil?
-            adapter.send(:remove_method, current_method)
-          else
-            position = thread[('history_logs'+id).to_sym][id+(adapter.to_s) + (current_method.to_s)].index(feature_selector)
-            adapter.send(:remove_method, current_method)
-            old_version = thread[('history'+id).to_sym][id+(adapter.to_s) + (current_method.to_s)][position+1]
-            adapter.send(:define_method, current_method, old_version)
-            thread[('history'+id).to_sym][id+(adapter.to_s) + (current_method.to_s)].delete_at(position+1)
-            thread[('history_logs'+id).to_sym][id+(adapter.to_s) + (current_method.to_s)].delete_at(position)
-          end
-        end
-      else
-        #We did not change any existing method
-
-        added_methods = moduler.instance_methods
-        added_methods.each do |current_method|
-          if adapter_methods.include? current_method
-            adapter.send(:remove_method, current_method)
-          else
-            puts 'This should not be reached' # Todo remove when debug ends
-          end
-        end
-
-      end
-      #$history_logs[adapter.to_s].delete_at(log_index)
 
     end
 
 
   end
 
-  # TODO: To be completed if needed
+  def integer_convert(string)
+    num = string.to_i
+    num if num.to_s == string
+  end
 
 end
