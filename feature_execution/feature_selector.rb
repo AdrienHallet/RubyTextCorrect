b00de3af6be4753de7c131ab508b570b5cc4cd6c
AdrienHalletUCL <adrien.hallet@student.uclouvain.be>
2018-03-18 01:03:45 +0100

    smallhelp

--- feature_execution/feature_selector.rb
+++ feature_execution/feature_selector.rb
@@ -1,5 +1,6 @@
 class FeatureSelector
   attr_reader :feature, :klass
+  attr_accessor :old_version
 
   def initialize(feature, klass)
     thread = Thread.current
@@ -8,6 +9,7 @@ class FeatureSelector
       thread[:history_logs] = Hash.new
     end
     @feature = Object.const_get(feature)
+    @old_version = Hash.new(nil)
 
     adapter = @feature.get_adapter.to_s
     raise "#{@feature} does not adapt from #{klass}" unless adapter.eql? klass
